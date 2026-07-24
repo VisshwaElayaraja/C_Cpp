86958f0c508ca627d15c585663ce63bfd1f2fcb7
Visshwa_Elayaraja <visshwainfo@gmail.com>
2026-07-24 12:39:43 +0530

    Initial Commit

---
+++ Learning/Alpha.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+int main(){
+
+    int x;
+    x = 67;
+
+    double y = 17.38;
+
+    double sum;
+    sum = x+y;
+
+    const std::string GENDER = "Male";
+    std::cout << "Gender: " << GENDER;
+
+    return 0;
+}
+// End of Code.
---
+++ Learning/Beta.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+
+int main(){
+    const double AREA = 987.85;
+    double height;
+    height = 5;
+    std::cout << "Area of building: " << AREA << " sq.meters\n";
+    std::cout << "Height of building: " << height << " meters\n";
+    
+    double volume = AREA * height;
+    std::cout << "Volume of building: " << volume << " cu.meters\n";
+
+    return 0;
+}
+// End of Code.
